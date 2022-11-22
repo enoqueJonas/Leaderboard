8dc7b6db3f31e7a7c695b5c536eb2a2180973ab7
enoqueJonas <enoque.macanda@gmail.com>
2022-11-22 19:51:14 +0200

    UPDATE index.js

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import './index.css';
+
+const scoresBodyDiv = document.querySelector('.recent-scores-body');
+const players = [
+  {
+    name: 'Name: ',
+    score: 100,
+  },
+  {
+    name: 'Name: ',
+    score: 30,
+  },
+  {
+    name: 'Name: ',
+    score: 55,
+  },
+  {
+    name: 'Name: ',
+    score: 87,
+  },
+  {
+    name: 'Name: ',
+    score: 12,
+  },
+  {
+    name: 'Name: ',
+    score: 233,
+  },
+  {
+    name: 'Name: ',
+    score: 74,
+  },
+];
+
+const loadPlayers = () => {
+  let playersList = '';
+  players.forEach((player, index) => {
+    playersList += `<p class="player ${index % 2 === 0 ? 'white' : 'grey'}">${player.name}: ${player.score}</p>`;
+  });
+  scoresBodyDiv.innerHTML = playersList;
+};
+
+window.onload = loadPlayers;
