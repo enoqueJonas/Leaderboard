f832c0848d4fd7dbbcf92809fc67f95f652c4a88
enoqueJonas <enoque.macanda@gmail.com>
2022-11-24 09:40:34 +0200

    FIX linters

--- src/index.js
+++ src/index.js
@@ -1,27 +1,14 @@
 import './index.css';
-import { btnSubmit, btnRefresh, nameInput, scoreInput } from './htmlElements.js';
-import { postScore, getScore } from './api';
-const lodash = require('lodash');
-const scoresBodyDiv = document.querySelector('.recent-scores-body');
+import {
+  btnSubmit, btnRefresh, nameInput, scoreInput,
+} from './htmlElements.js';
+import { postScore, getScore } from './api.js';
 
-async function createGame(url) {
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    mode: 'no-cors',
-    body: {
-      "name": "Moneting.net"
-    },
-  });
-  return response.json();
-}
+const scoresBodyDiv = document.querySelector('.recent-scores-body');
 
 const loadPlayers = async () => {
   let playersList = '';
-  let play = await getScore();
-  console.log(play)
+  const play = await getScore();
   play.forEach((player, index) => {
     playersList += `<p class="player ${index % 2 !== 0 ? 'grey' : ''}">${player.user}: ${player.score}</p>`;
   });
@@ -29,12 +16,12 @@ const loadPlayers = async () => {
 };
 
 const btnSubmitEvent = () => {
-  let player = {};
+  const player = {};
   player.score = Number(scoreInput.value);
   player.user = nameInput.value;
-  postScore(player).then(data => console.log(data));
-}
+  postScore(player);
+};
 
 window.onload = loadPlayers;
 btnSubmit.addEventListener('click', btnSubmitEvent);
-btnRefresh.addEventListener('click', loadPlayers)
+btnRefresh.addEventListener('click', loadPlayers);
